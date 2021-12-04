980b662b50fbb0b249f752d7946e782f5c8d1729
PrateekPathak9002 <89402723+PrateekPathak9002@users.noreply.github.com>
2021-12-04 18:50:10 +0530

    Added debug options

--- search.js
+++ search.js
@@ -1,29 +1,39 @@
-function search(searchOBJS,input) {
 
-	if (searchOBJS.includes('#')) {/*Checks that the second arguement is a class .*/
+function search_debug(searchOBJS,sOBJS,inputLc,input) {
+	if (searchOBJS.includes('#')) {/*Checks that the first arguement is a class .*/
 		console.error('search.js: search function only accepts an class as second argument')
 		return false
 	}
 
-	let sOBJS = document.querySelectorAll(searchOBJS)
 	if (sOBJS[0] == undefined) {
-		console.warning('search.js: The given class does not have any elements associated with it .')
+		console.warn('search.js: The given class does not have any elements associated with it .')
 		return false
 	}
 
-
-	let inputLc = document.querySelectorAll(input)[0] /*Lc stands for local*/
 	if (inputLc == undefined) {
-		console.error('search.js: The given id for the input does not have a input associated with it .')
+		console.error('search.js: The given id for the input does not have a element associated with it .')
+		return false
+	}
+	
+	console.log('search.js :No errors in the arguments .')
+	
+}
+
+function search(searchOBJS,input,options={debug:true}) {
+
+	let sOBJS = document.querySelectorAll(searchOBJS)
+	let inputLc = document.querySelectorAll(input)[0] /*Lc stands for local*/
+
+	if (options.debug) {
+		return search_debug(searchOBJS,sOBJS,inputLc,input)
 	}
 
-	console.log('Search :No errors in the arguments .')
 	inputLc.addEventListener('input',()=>{
 		for(var i = 0 ; i < sOBJS.length ; i++ ){
 			if (!(sOBJS[i].innerHTML.includes(inputLc.value))) {
 				sOBJS[i].style.position = 'absolute'
 				sOBJS[i].style.visibility = 'hidden'
-				console.log(`${sOBJS[i].innerHTML} and input is ${inputLc.value} `)
+				
 			}else {
 				sOBJS[i].style.position = 'relative'
 				sOBJS[i].style.visibility = 'visible'
@@ -32,3 +42,4 @@ function search(searchOBJS,input) {
 		
 	})
 }
+
